7b75f9dce5d38793a66c0e8b0ae30c63dd9c3757
tensae12 <tensaewerkeye224@gmail.com>
2024-03-31 00:01:37 -0700

    pagination with crawl template

--- spiderP/spiders/transcripts.py
+++ spiderP/spiders/transcripts.py
@@ -6,11 +6,20 @@ from scrapy.spiders import CrawlSpider, Rule
 class TranscriptsSpider(CrawlSpider):
     name = "transcripts"
     allowed_domains = ["subslikescript.com"]
-    start_urls = ["https://subslikescript.com"]
+    start_urls = ["https://subslikescript.com/movies_letter-X"]
 
     rules = (
         Rule(LinkExtractor(restrict_xpaths=("//ul[@class='scripts-list']/a")), callback="parse_item", follow=True),
+        Rule(LinkExtractor(restrict_xpaths=("(//a[@rel='next'])[1]"))),
+
         )
 
     def parse_item(self, response):
-       print(response.url)
+        article = response.xpath("//article[@class='main-article']")
+        yield {
+            'title' : article.xpath("./h1/text()").get(),
+            'plot' : article.xpath("./p/text()").get(), 
+            'transcript' : article.xpath("./div[@class='full-script']").getall(),
+            'url' : response.url,
+
+        }
